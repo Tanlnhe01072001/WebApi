481df6b089b26a940bdf32600c1382be6aa8f912
Nhat Tan <tanln01072001@gmail.com>
2024-06-05 23:10:15 +0700

    Gan hoan thien

---
+++ MyWebApiApp/MyWebApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HanggHoa> hanghoas = new List<HanggHoa>();
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Ok(hanghoas);
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hanghoaVM)
+        {
+            var hanghoa = new HanggHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hanghoaVM.TenHangHoa,
+                DonGia = hanghoaVM.DonGia,
+            };
+            hanghoas.Add(hanghoa);
+            return
+        }
+    }
+}
